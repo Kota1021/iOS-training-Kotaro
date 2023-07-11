de198c0fc3142fedd9b7196ead760b439163565a
Kotaro Matsumoto <ko_matsumoto@yumemi.co.jp>
2023-07-11 14:10:02 +0900

    made pdf's color changeable

--- iOS-training/LayoutPrototype.swift
+++ iOS-training/LayoutPrototype.swift
@@ -6,19 +6,45 @@
 //
 
 import SwiftUI
+import YumemiWeather
 
 struct LayoutPrototype: View {
+    @State var fetchedWeather: Weather? = nil
+
     var body: some View {
         GeometryReader { geometry in
+
             let imageSideLength = geometry.size.width / 2
             let temperatureWidth = geometry.size.width / 4
             let buttonWidth = geometry.size.width / 4
 
             VStack(alignment: .center, spacing: .zero) {
-                Image(systemName: "photo.fill")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: imageSideLength, height: imageSideLength)
+                switch fetchedWeather {
+                case .sunny:
+                    Image(.iconsun)
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundStyle(.red)
+                        .frame(width: imageSideLength, height: imageSideLength)
+                case .cloudy:
+                    Image(.iconclouds)
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundStyle(.gray)
+                        .frame(width: imageSideLength, height: imageSideLength)
+                case .rainy:
+                    Image(.iconumbrella)
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundStyle(.blue)
+                        .frame(width: imageSideLength, height: imageSideLength)
+                case .none:
+                    Image(systemName: "exclamationmark.square.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundStyle(.gray)
+                        .frame(width: imageSideLength, height: imageSideLength)
+                }
 
                 HStack(spacing: .zero) {
                     Text("--")
@@ -33,8 +59,19 @@ struct LayoutPrototype: View {
                 HStack(spacing: .zero) {
                     Button("Close") {}
                         .frame(width: buttonWidth)
-                    Button("Reload") {}
-                        .frame(width: buttonWidth)
+                    Button("Reload") {
+                        let weather = YumemiWeather.fetchWeatherCondition()
+                        // FIXME: Swift5.9からswitchで値を返せるはずだが、以下のようにクロージャで囲わないとビルドはできてもプレビューでCommand SwiftCompile failed...とエラー発生
+                        fetchedWeather = {
+                            switch weather {
+                            case "sunny": .sunny
+                            case "cloudy": .cloudy
+                            case "rainy": .rainy
+                            default: fatalError("invalid weather: \(weather)")
+                            }
+                        }()
+                    }
+                    .frame(width: buttonWidth)
                 }
             }
             .frame(width: geometry.size.width, height: geometry.size.height)
@@ -44,4 +81,10 @@ struct LayoutPrototype: View {
 
 #Preview {
     LayoutPrototype()
+}
+
+enum Weather {
+    case sunny
+    case cloudy
+    case rainy
 }
