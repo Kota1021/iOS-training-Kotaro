4f8a6da7e33300128764cd6bd6240f58d2f9a1a3
Kotaro Matsumoto <ko_matsumoto@yumemi.co.jp>
2023-07-11 16:23:48 +0900

    modified indents and space. removed spacer above buttons, and added padding instead.

--- iOS-training/LayoutPrototype.swift
+++ iOS-training/LayoutPrototype.swift
@@ -9,37 +9,30 @@ import SwiftUI
 
 struct LayoutPrototype: View {
     var body: some View {
-            GeometryReader{ geometry in
-                VStack(alignment: .center, spacing: .zero) {
-                
-                    Image(systemName: "photo.fill")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: geometry.size.width/2,height: geometry.size.width/2)
-                        .background(
-                            Rectangle()
-                                .foregroundStyle(.yellow)
-                                )
-                    
-                    HStack{
-                            Text("--")
-                                .foregroundStyle(.blue)
-                                .frame(width: geometry.size.width/4)
-                            Text("--")
-                                .foregroundStyle(.red)
-                                .frame(width: geometry.size.width/4)
-                    }
-                    
-                    Spacer().frame(height: 80)
-                    
-                    HStack{
-                        Button("Close"){}
-                            .frame(width: geometry.size.width/4)
-                        Button("Reload"){}
-                            .frame(width: geometry.size.width/4)
-                    }
-                }.frame(width: geometry.size.width, height: geometry.size.height)
-            }
+        GeometryReader { geometry in
+            VStack(alignment: .center, spacing: .zero) {
+                Image(systemName: "photo.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: geometry.size.width / 2, height: geometry.size.width / 2)
+
+                HStack {
+                    Text("--")
+                        .foregroundStyle(.blue)
+                        .frame(width: geometry.size.width / 4)
+                    Text("--")
+                        .foregroundStyle(.red)
+                        .frame(width: geometry.size.width / 4)
+                }.padding(.bottom, 80)
+
+                HStack {
+                    Button("Close") {}
+                        .frame(width: geometry.size.width / 4)
+                    Button("Reload") {}
+                        .frame(width: geometry.size.width / 4)
+                }
+            }.frame(width: geometry.size.width, height: geometry.size.height)
+        }
     }
 }
 
