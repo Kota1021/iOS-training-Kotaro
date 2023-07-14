1b5eb15d33657904edd78f06fb846dae1c9cdc49
Kota1021 <Kota1021@users.noreply.github.com>
2023-07-14 13:50:32 +0900

    deleted SwiftUI Reference from Weather model

--- iOS-training/Models/Weather.swift
+++ iOS-training/Models/Weather.swift
@@ -5,32 +5,9 @@
 //  Created by 松本 幸太郎 on 2023/07/12.
 //
 
-import SwiftUI
 
 enum Weather: String, Decodable {
     case sunny
     case cloudy
     case rainy
-
-    var icon: Image {
-        switch self {
-        case .sunny:
-            return Image(.iconsun)
-        case .cloudy:
-            return Image(.iconclouds)
-        case .rainy:
-            return Image(.iconumbrella)
-        }
-    }
-
-    var color: Color {
-        switch self {
-        case .sunny:
-            return .red
-        case .cloudy:
-            return .gray
-        case .rainy:
-            return .blue
-        }
-    }
 }
