4bb584875000ce5f7bf4bfdf2e2ca043dcbe747f
KotaroMatsumoto <ko_matsumoto@yumemi.co.jp>
2023-09-29 16:09:00 +0900

    fixed the random weather logic in WeatherAPIStub

--- iOS-training/Preview Content/WeatherAPIStub.swift
+++ iOS-training/Preview Content/WeatherAPIStub.swift
@@ -9,13 +9,14 @@ import Foundation
 import YumemiWeather
 
 struct WeatherAPIStub: WeatherAPI {
-    let weather: Weather
+    let weather: Weather?
     init(_ weather: Weather? = nil) {
-        self.weather = weather ?? Weather.allCases.randomElement()!
+        self.weather = weather
     }
 
     func fetchWeatherCondition(in _: String, at _: Date) -> Result<WeatherDateTemperature, Error> {
         let date = Date(timeIntervalSince1970: 0)
+        let weather = weather ?? Weather.allCases.randomElement()!
         let weatherDateTemperature = WeatherDateTemperature(maxTemperature: 30,
                                                             date: date,
                                                             minTemperature: 20,
