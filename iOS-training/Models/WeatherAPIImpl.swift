c6a90c8f46bceed558aad8a85227ca573ddf71a7
Kota1021 <Kota1021@users.noreply.github.com>
2023-10-13 11:44:14 +0900

    added Generators for JSON to Object, and Object to JSON
    # Conflicts:
    #	iOS-training/Models/WeatherAPIImpl.swift

--- iOS-training/Models/WeatherAPIImpl.swift
+++ iOS-training/Models/WeatherAPIImpl.swift
@@ -15,9 +15,9 @@ struct WeatherAPIImpl: WeatherAPI {
         let weatherDateTemperature = Result<WeatherDateTemperature, Error> {
             // MARK: Decoding from output JSON String
 
-            let requestJSON = try WeatherRequestEncoder().generateRequestJSON(area: area, date: date)
+            let requestJSON = try WeatherRequestGenerator().generate(area: area, date: date)
             let fetchedWeatherJSON = try YumemiWeather.fetchWeather(requestJSON) // can throw YumemiWeatherError.invalidParameterError and \.unknownError
-            let weatherDateTemperature = try WeatherDateTemperatureDecoder().generateWeatherDateTemperature(from: fetchedWeatherJSON)
+            let weatherDateTemperature = try WeatherDateTemperatureGenerator().generate(from: fetchedWeatherJSON)
             return weatherDateTemperature
         }
 
@@ -33,18 +33,21 @@ private let dateFormatter: DateFormatter = {
     return dateFormatter
 }()
 
-struct WeatherRequestEncoder {
-    func generateRequestJSON(area: String, date: Date) throws -> String {
+struct WeatherRequestGenerator {
+    private let encoder: JSONEncoder = {
         let encoder = JSONEncoder()
         encoder.dateEncodingStrategy = .formatted(dateFormatter)
         encoder.outputFormatting = .sortedKeys
-
+        return encoder
+    }()
+    
+    func generate(area: String, date: Date) throws -> String {
         let areaDateJSONData = try encoder.encode(AreaDate(area: area, date: date))
         let areaDateJSON = String(data: areaDateJSONData, encoding: .utf8)
-
         guard let areaDateJSON else {
             throw JSONError.failedToStringify
         }
+
         return areaDateJSON
     }
 
@@ -64,14 +67,17 @@ struct WeatherRequestEncoder {
     }
 }
 
-struct WeatherDateTemperatureDecoder {
+struct WeatherDateTemperatureGenerator {
     // MARK: - output
 
-    func generateWeatherDateTemperature(from json: String) throws -> WeatherDateTemperature {
+    private let decoder: JSONDecoder = {
         let decoder = JSONDecoder()
         decoder.dateDecodingStrategy = .formatted(dateFormatter)
         decoder.keyDecodingStrategy = .convertFromSnakeCase
-
+        return decoder
+    }()
+    
+    func generate(from json: String) throws -> WeatherDateTemperature {
         let weatherDateTemperature = try decoder.decode(WeatherDateTemperature.self, from: Data(json.utf8))
         return weatherDateTemperature
     }
